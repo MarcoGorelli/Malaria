6ab9e1869b8455cf4631a222e9605cc601c4cd26
MarcoGorelli <m.e.gorelli@gmail.com>
2019-08-25 18:01:10 +0100

    Revert to seaborn (for the time being)

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
-DATA = pd.read_csv("../data/malaria-deaths-by-region.csv")
+DATA = pd.read_csv("../data/malaria-deaths-by-region.csv").rename(
+    {
+        "Deaths - Malaria - Sex: Both - Age: All Ages (Number) (deaths)": "Deaths by malaria"
+    },
+    axis=1,
+)
--- src/visualiser.py
+++ src/visualiser.py
@@ -8,8 +8,6 @@ sns.set()
 def plot_country_deaths_over_time(country):
     return (
         DATA.query("Entity == '{}'".format(country))
-        .set_index("Year")[
-            ["Deaths - Malaria - Sex: Both - Age: All Ages (Number) (deaths)"]
-        ]
-        .plot()
+        .set_index("Year")[["Deaths by malaria"]]
+        .plot(figsize=(12, 8))
     )
