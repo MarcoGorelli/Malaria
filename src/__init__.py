816a144a432e5066e34741733cf7a5e05e662f09
MarcoGorelli <m.e.gorelli@gmail.com>
2019-08-25 17:06:06 +0100

    Add requirements, visualiser functoin

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+DATA = pd.read_csv("../data/malaria-deaths-by-region.csv")
+
---
+++ src/visualiser.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+
+from src import DATA
+
+sns.set()
+
+
+def plot_country_deaths_over_time(country):
+    return (
+        DATA.query("Entity == '{}'".format(country))
+        .set_index("Year")[
+            ["Deaths - Malaria - Sex: Both - Age: All Ages (Number) (deaths)"]
+        ]
+        .plot()
+    )
