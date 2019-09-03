d2f61c85d12a32b9ecc0a03878979ce60c65cc31
MarcoGorelli <m.e.gorelli@gmail.com>
2019-09-03 22:16:39 +0100

    UI changes

--- src/__init__.py
+++ src/__init__.py
@@ -6,3 +6,7 @@ DEATHS = pd.read_csv("data/malaria-deaths-by-region.csv").rename(
     },
     axis=1,
 )
+CODES_AND_CONTINENTS = pd.read_csv("data/country-and-continent-codes-list.csv").rename(
+    {"Three_Letter_Country_Code": "Code"}, axis=1
+)
+DEATHS = CODES_AND_CONTINENTS[["Code", "Continent_Code"]].merge(DEATHS, on="Code")
