7df52dca92d5292245cee376f40d7c715984539e
MarcoGorelli <m.e.gorelli@gmail.com>
2019-09-08 11:27:50 +0100

    remove old tests, wip

--- tests/test_coordinates.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from src import coordinates
-
-
-@pytest.mark.parametrize("country, expected", [(), ()])
-def test_get_coordinates(country, expected):
-    assert coordinates.get_coordinates(country) == expected
